3b251454c0c2c50f5fd72c861ae44913b6e4280e
nFrakir <n.frakir@gmail.com>
2020-10-13 20:43:09 +0300

    Change stack inner data type from List to Array

--- src/main/java/com/gmail/seliverstova/hanna/BlackList.java
+++ src/main/java/com/gmail/seliverstova/hanna/BlackList.java
@@ -1,22 +1,55 @@
 package com.gmail.seliverstova.hanna;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class BlackList {
-    private List<Class<?>> blackList = new ArrayList<>();
+    private Class<?>[] blackList = new Class<?>[10];
 
     public BlackList() {
         super();
     }
 
     public void addToBlackList(Object obj) {
-        if (!blackList.contains((Object) obj.getClass())) {
-            blackList.add(obj.getClass());
+        if (!findElement(obj.getClass())) {
+            add(obj.getClass());
         }
     }
 
     public boolean isOpjectClassPresent(Object obj) {
-        return blackList.contains((Object) obj.getClass());
+        return findElement(obj.getClass());
+    }
+
+    private void resize() {
+        Class<?>[] tmp = Arrays.copyOf(blackList, (blackList.length + 10));
+        blackList = tmp;
+    }
+
+    private int getLastIndex() {
+        int index = -1;
+        for (int i = 0; i < blackList.length; i++) {
+            if (blackList[i] == null) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    private void add(Class<?> obj) {
+        int index = getLastIndex();
+        if (index == -1) {
+            index = blackList.length;
+            resize();
+        }
+        blackList[index] = obj;
+    }
+
+    private boolean findElement(Class<?> neddle) {
+        for (Class<?> element : blackList) {
+            if (neddle == element) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/com/gmail/seliverstova/hanna/Stack.java
+++ src/main/java/com/gmail/seliverstova/hanna/Stack.java
@@ -1,11 +1,10 @@
 package com.gmail.seliverstova.hanna;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Stack {
-    private List<Object> stack = new ArrayList<>();
     private BlackList blackList;
+    private Object[] stack = new Object[10];
 
     public Stack(BlackList blackList) {
         super();
@@ -16,20 +15,32 @@ public class Stack {
         super();
     }
 
+    public Object[] getStack() {
+        return stack;
+    }
+
+    public void setStack(Object[] stack) {
+        this.stack = stack;
+    }
+
     public Object removeObject() {
-        if (stack.isEmpty()) {
+        int index = getLastIndex();
+        if (index == -1) {
             return null;
         }
-        Object obj = stack.get(stack.size() - 1);
-        stack.remove(stack.size() - 1);
+        index = (index == 0) ? 0 : (index - 1);
+        Object obj = stack[index];
+        stack[index] = null;
         return obj;
     }
 
     public Object getObject() {
-        if (stack.isEmpty()) {
+        int index = getLastIndex();
+        if (index == -1) {
             return null;
         }
-        Object obj = stack.get(stack.size() - 1);
+        index = (index == 0) ? 0 : (index - 1);
+        Object obj = stack[index];
         return obj;
     }
 
@@ -37,6 +48,31 @@ public class Stack {
         if (blackList.isOpjectClassPresent(obj)) {
             throw new IllegalArgumentException("Class of this object is in black list");
         }
-        stack.add(obj);
+        add(obj);
+    }
+
+    private void add(Object obj) {
+        int index = getLastIndex();
+        if (index == -1) {
+            index = stack.length;
+            resize();
+        }
+        stack[index] = obj;
+    }
+
+    private void resize() {
+        Object[] tmp = Arrays.copyOf(stack, (stack.length + 10));
+        stack = tmp;
+    }
+
+    private int getLastIndex() {
+        int index = -1;
+        for (int i = 0; i < stack.length; i++) {
+            if (stack[i] == null) {
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 }
